4e670804ce72be2daffc2667e5099d6bfad2d38b
Dhikshita <dhikshita.macher@tigeranalytics.com>
2024-03-19 14:12:12 +0530

    Executed the nonstandardcode.py

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -2,7 +2,7 @@ import os
 import tarfile
 
 # import matplotlib as mpl
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.stats import randint
@@ -38,15 +38,16 @@ def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-
-housing = load_housing_data
-train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+fetch_housing_data()
+housing = load_housing_data()
 
 housing["income_cat"] = pd.cut(
     housing["median_income"],
     bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
     labels=[1, 2, 3, 4, 5],
 )
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
@@ -79,7 +80,7 @@ housing = strat_train_set.copy()
 housing.plot(kind="scatter", x="longitude", y="latitude")
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
 
-corr_matrix = housing.corr()
+corr_matrix = housing.corr(numeric_only=True)
 corr_matrix["median_house_value"].sort_values(ascending=False)
 housing["rooms_per_household"] = housing["total_rooms"] / housing["households"]
 housing["bedrooms_per_room"] = (
