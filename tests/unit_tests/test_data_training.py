9311e5bfcd68248bc69ba0e476c61219816eab91
Dhikshita <dhikshita.macher@tigeranalytics.com>
2024-03-28 12:46:56 +0530

    Add new unit test

--- tests/unit_tests/test_data_training.py
+++ tests/unit_tests/test_data_training.py
@@ -9,7 +9,7 @@ from housePricePrediction import data_training
 class TestModelTraining(unittest.TestCase):
     def test_stratifiedShuffleSplit(self):
         self.data = pd.DataFrame({
-            'income_cat': [1, 2, 3, 4, 3, 1, 2, 4, 5, 1],
+            'income_cat': [[1], [2], [3], [4], [3], [1], [2], [4], [5], [1]],
             'total_rooms': [50, 100, 60, 70, 200, 150, 200, 300, 100, 100],
             'households': [60, 10, 20, 40, 50, 35, 40, 25, 10, 25]})
         train_set, test_set, _, _ = \
