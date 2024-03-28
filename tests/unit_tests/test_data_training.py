61e32985eebb99fd9eeeac0b10d9fda6bc2fed8a
Dhikshita <dhikshita.macher@tigeranalytics.com>
2024-03-28 13:02:17 +0530

    Add new unit test

--- tests/unit_tests/test_data_training.py
+++ tests/unit_tests/test_data_training.py
@@ -14,6 +14,15 @@ class TestModelTraining(unittest.TestCase):
         pred = data_training.train_data_regression('lin', self.X, self.y)
         self.assertAlmostEqual(pred[0], 1)
 
+    def test_stratifiedShuffleSplit(self):
+        self.data = pd.DataFrame({
+            'income_cat': [1, 2, 3, 4, 3, 1, 2, 4, 5, 1],
+            'total_rooms': [50, 100, 60, 70, 200, 150, 200, 300, 100, 100],
+            'households': [60, 10, 20, 40, 50, 35, 40, 25, 10, 25]})
+        train_set, test_set, strain, stest = \
+            data_training.stratified_Shuffle_Split(self.data)
+        self.assertEqual(strain.shape[0], 8)
+        #self.assertEqual(test_set.shape[0], 2)
 
 
 
