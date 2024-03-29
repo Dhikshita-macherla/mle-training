b440a517b9bcf0a037c39bb2c514ab19182389c1
Dhikshita <dhikshita.macher@tigeranalytics.com>
2024-03-29 10:52:52 +0530

    Added new testcase for test_stratifiedShuffleSplit

--- tests/unit_tests/test_data_training.py
+++ tests/unit_tests/test_data_training.py
@@ -1,14 +1,27 @@
 import unittest
 
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
 
-from housePricePrediction import data_ingestion, data_training
+from housePricePrediction import data_training
 
 
 class TestModelTraining(unittest.TestCase):
     def test_stratifiedShuffleSplit(self):
-        self.data = data_ingestion.load_housing_data()
+        self.data = {
+            'longitude': [-122.23, -122.22, -122.24, -122.25, -122.25],
+            'latitude': [37.88, 37.86, 37.85, 37.85, 37.85],
+            'housing_median_age': [41, 21, 52, 52, 52],
+            'total_rooms': [880, 7099, 1467, 1274, 1627],
+            'total_bedrooms': [129, 1106, 190, 235, 280],
+            'population': [322, 2401, 496, 558, 565],
+            'households': [126, 1138, 177, 219, 259],
+            'median_income': [8.3252, 8.3014, 7.2574, 5.6431, 3.8462],
+            'ocean_proximity': ['NEAR BAY',
+                                'NEAR BAY', 'NEAR BAY', 'NEAR BAY', 'NEAR BAY']
+            }
+
+        self.data = pd.DataFrame(self.data)
         train_set, test_set, strain, stest = \
             data_training.stratified_Shuffle_Split(self.data)
         self.assertGreaterEqual(len(set(strain)), 1)
@@ -23,4 +36,5 @@ class TestModelTraining(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    unittest.main()
     unittest.main()
